0dd9010466b520d6a46b7e80a3b2914a4b375fb0
Kim Miso <dutlsalth9811@gmail.com>
2022-09-25 18:45:30 +0900

    Create project

---
+++ careerForDeveloper/src/main/java/com/example/careerForDeveloper/CareerForDeveloperApplication.java
@@ -0,0 +1,17 @@
+package com.example.careerForDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerForDeveloperApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CareerForDeveloperApplication.class, args);
+		// 메모리 사용량 출력
+		long heapSize = Runtime.getRuntime().totalMemory();
+		System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
+	}
+
+}
---
+++ careerForDeveloper/src/test/java/com/example/careerForDeveloper/CareerForDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.careerForDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerForDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
