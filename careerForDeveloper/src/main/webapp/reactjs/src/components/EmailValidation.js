fcf3812f09a058d9f7ee8deed73cb78edd65bacf
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-14 10:10:30 +0900

    feat: 회원가입 구현

---
+++ careerForDeveloper/src/main/webapp/reactjs/src/components/EmailValidation.js
@@ -0,0 +1,38 @@
+import react from "react";
+import axios from "axios";
+import React from "react";
+
+function EmailValidation() {
+    const [code, setCode] = React.useState("");
+
+    const validateCode = (e) => {
+        e.preventDefault();
+        if (!code) {
+            return alert("인증번호를 입력하세요.");
+        }
+        const params = new URLSearchParams();
+        params.append('certifiedCode', code);
+        console.log(code);
+        // axios.post("/auth", params)
+        //     .then((res) => {
+        //         console.log(res);
+        //         if (res.data.isSuccess) {
+        //             alert("인증에 성공하였습니다.");
+        //         }
+        //         else {
+        //             alert("인증번호가 다릅니다.");
+        //         }
+        //     });
+    }
+
+    return (
+        <>
+            <label htmlFor="code">인증번호:</label>
+            <input type="text" id="code" value={code} onChange={(e) => setCode(e.target.value)} />
+            <button onClick={validateCode}>인증</button>
+            <br />
+        </>
+    )
+}
+
+export default EmailValidation;
